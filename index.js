2afa9aebdf4d6d276933d561614ba4f656710b1a
Jacob Sendzik <jake.sendzik@gmail.com>
2023-10-03 14:45:50 -0400

    setting up fetch

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => console.log("Connected"))
+//Do next 
+//get a working fetch request from a db.json file
+//replace above console.log with fetch function
+function fetchData() {
+    fetch("url")
+    .then((resp) => resp.json())
+    .then((data) => console.log(data))
+
+}
