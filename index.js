f5adf5bf5a8b0be3409b56f977ce14a8ec82eae5
Jacob Sendzik <jake.sendzik@gmail.com>
2023-10-04 16:46:39 -0400

    json changes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => console.log("Connected"))
 //Do next 
 //get a working fetch request from a db.json file
 //replace above console.log with fetch function
-function fetchData() {
-    fetch("url")
+function fetchData(url) {
+    fetch(url)
     .then((resp) => resp.json())
     .then((data) => console.log(data))
 
