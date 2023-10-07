7c6dbc2da98d6f0ac319832945df92b6c1dd230b
Jacob Sendzik <jake.sendzik@gmail.com>
2023-10-07 12:59:46 -0400

    building html

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Connected");
     fetchData("http://localhost:3000/questions")
 })
-//Do next 
-//get a working fetch request from a db.json file
-//replace above console.log with fetch function
+
 function fetchData(url) {
     fetch(url)
     .then((resp) => resp.json())
     .then((data) => console.log(data))
+}
 
+const header = document.querySelector('h1')
+header.style.fontSize = "40px"
+
+const list = document.querySelector("#questionList")
+
+
+const startButton = document.querySelector("#start")
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+    fetch("http://localhost:3000/questions")
+    .then((resp) => resp.json())
+    .then((questions) => post(questions))
+}
+
+function post(questions) {
+    questions.forEach((item) => {
+        const p = document.createElement("p")
+        list.appendChild(p);
+        p.innerText = item.question;
+    })
 }
